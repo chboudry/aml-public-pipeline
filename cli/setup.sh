e2555eda3a6ecccf627b2cf3979c1190d2dd767f
Charles Boudry <chboudry@outlook.com>
2022-11-30 19:35:52 +0100

    Update setup.sh

--- cli/setup.sh
+++ cli/setup.sh
@@ -31,6 +31,12 @@ LOCATION="$LOCATION"
 WORKSPACE="$WORKSPACE_NAME"
 # </set_variables>
 
+if [ "$RG_NAME" == "onlineml" ]; then
+  echo "that's the one!"
+else
+  echo "secret does not match"
+fi
+
 # If RESOURCE_GROUP_NAME is empty, the az configure is pending.
 RESOURCE_GROUP_NAME=${RESOURCE_GROUP_NAME:-}
 if [[ -z "$RESOURCE_GROUP_NAME" ]]
